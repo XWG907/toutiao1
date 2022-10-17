9b18ebfd7fc311c9200acfd7e59ed4717fc2d671
XWG907 <18807255050@163.com>
2022-10-17 15:53:23 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import './style/index.less'// 引入初始化样式
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.resarch.itcast.cn/mp/v1.0'
+Vue.prototype.$axios = axios // 给所有vue对象的原型属性赋值 那么所有vue实例自动拥有$axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)// 全局注册
 
