3290dd92fc492b64a1e330a77768d9e894463c5d
XWG907 <18807255050@163.com>
2022-10-17 15:09:30 +0800

    登录网页的表单数据绑定和校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,23 +5,24 @@
       <div class="login_img">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
+
       <!-- 表单组件 -->
-      <el-form>
+      <el-form :model="loginForm" :rules="rules" ref='loginForm'>
         <!-- 表单项 -->
-        <el-form-item>
+        <el-form-item prop="mobile">
           <!-- 放置组件项0 -->
-          <el-input></el-input>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- 放置组件项0 -->
-          <el-input style="width:280px"></el-input>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:280px"></el-input>
           <el-button style= "float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议</el-checkbox>
+        <el-form-item prop="check">
+          <el-checkbox  v-model="loginForm.check">我已阅读并同意用户协议</el-checkbox>
         </el-form-item>
-        <el-form-item>
-          <el-button  type="primary"  style="width:100%">登录</el-button>
+        <el-form-item prop="">
+          <el-button @click="login" type="primary"  style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
 
@@ -33,6 +34,54 @@
 export default {
   data () {
     return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      // 表单验证规则
+      rules: {
+        mobile: [{
+          required: true,
+          message: '手机号不能为空'
+        }, {
+          pattern: /^1[3456789]\d{9}$/,
+          message: '手机号格式不正确'
+        }
+        ],
+        code: [{
+          required: true,
+          message: '验证码不能为空'
+        }, {
+          pattern: /^\d{6}$/,
+          message: '验证码长度必须为6个数字',
+          trigger: 'blur'
+        }
+        ],
+        check: [{
+          validator: this.loginFormRule
+        }]
+      }
+    }
+  },
+  methods: {
+    // 自定义验证函数 判读一些值是否正确
+    loginFormRule (rule, value, callBack) {
+      if (value) {
+        callBack()
+      } else {
+        callBack(new Error('你必须无条件同意'))
+      }
+    },
+    login () {
+      // 通过rem获取到elform的dom对象利用表单el-form组件的validate方法来校验整个表单的数据
+      this.$refs.loginForm.validate((isOK) => {
+        if (isOK) {
+          this.$message({ type: 'success', message: '成功' })
+        } else {
+          this.$message({ type: 'warning', message: '失败' })
+        }
+      })
     }
   }
 }
